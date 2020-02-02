5800fc652035961d72eef627f06d4c0ea6c92dbb
Rupu <rupuislamashraful7@gmail.com>
2020-02-02 17:34:47 +0600

    be RUPU  card with swiftUI.

--- bR Card/ContentView.swift
+++ bR Card/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red:0.20, green:0.29, blue:0.37)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("rupu")
+                .resizable()
+                    .aspectRatio(contentMode:.fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.white, lineWidth: 5)
+                )
+                Text("be RUPU")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("ios Developer")
+                .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfiView(text: "01303599344", imageName: "phone.fill")
+                InfiView(text: "berupu@gmail.com", imageName: "envelope.fill")
+                
+                
+                
+                
+                
+                
+            }
+            
+        }
+        
     }
 }
 
@@ -19,3 +51,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
